5375aecab5360dbc932c608a6d9dc1069966c70e
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-03 15:13:42 +0100

    Add App structure

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,22 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    tasks: []
+  };
+
   render() {
-    return <p>Hello World!</p>;
+    return (
+      <section className="content">
+        <h1 className="content-title">TO DO LIST</h1>
+        <div className="content-btns">
+          {/* <Btn/> */}
+          {/* <Btn/> */}
+          {/* <Btn/> */}
+        </div>
+        {/* <TaskList/> */}
+      </section>
+    );
   }
 }
 
